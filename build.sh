3b07ed6475c6143bcf896f2c9416c36d8a2cac24
Vaclav Petras <wenzeslaus@gmail.com>
2015-08-18 16:25:27 -0400

    port of the text from PPT slides from 2012
    http://courses.ncsu.edu/mea582/common/GIS_anal_lecture/LOSGEO_GRASS.ppt
    Brief overview of FOSS4G: Free and Open Source Software for Geoinformatics
    Geospatial Analysis and Modeling: Open Source Software for GIS
    MEA592
    Helena Mitasova

---
+++ build.sh
@@ -0,0 +1,6 @@
+./copy-common-files.py --dst-dir=build
+./build-slides.py --outdir=build \
+    --title="Open Source Software for GIS" \
+    --meta-description="Geospatial Analysis and Modeling: Open Source Software for GIS" \
+    --meta-author="Helena Mitasova" \
+    index.html
