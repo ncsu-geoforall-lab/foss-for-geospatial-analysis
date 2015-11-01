10d2671fdc23b9c86aa88489de651ca9c4b13b39
Vaclav Petras <wenzeslaus@gmail.com>
2015-10-31 22:35:41 -0400

    slides for foss-in-geospatial-science-education
    A simplified version of Using Free and Open Source Solutions
    in Geospatial Science Education with basic formating
    and pictures.
    
    Removed the Standardized Sample Dataset part
    and pictures.

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@
     --meta-description="Introduction to Free, Libre and Open Source Software: for Geospatial Analysis and Modeling" \
     --meta-author="Helena Mitasova and Vaclav Petras" \
     index.html
+
+./build-slides.py --outdir=build \
+    --title="Integrating FOSS into GIScience Curriculum & Research" \
+    --meta-description="Integrating free, libre and open source software to GIScience, geospatial analysis, modeling and research" \
+    --meta-author="Helena Mitasova and Vaclav Petras" \
+    --outfile=integrating-foss.html \
+    integrating-foss.html
