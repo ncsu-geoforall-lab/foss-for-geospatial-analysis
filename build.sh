ac4e18beefe4c88d718e0cca653596ef7c50a871
Vaclav Petras <wenzeslaus@gmail.com>
2015-11-03 22:44:47 -0500

    rename presentation files

--- build.sh
+++ build.sh
@@ -10,12 +10,16 @@
     --title="Introduction to FOSS4G" \
     --meta-description="Introduction to Free, Libre and Open Source Software for Geospatial Analysis and Modeling" \
     --meta-author="Helena Mitasova and Vaclav Petras" \
-    --outfile=intro-to-foss.html \
-    intro-to-foss.html
+    --outfile=introduction.html \
+    introduction.html
 
 ./build-slides.py --outdir=build \
     --title="Integrating FOSS into GIScience Curriculum & Research" \
     --meta-description="Integrating free, libre and open source software to GIScience, geospatial analysis, modeling and research" \
     --meta-author="Helena Mitasova and Vaclav Petras" \
-    --outfile=integrating-foss.html \
-    integrating-foss.html
+    --outfile=integrating.html \
+    integrating.html
+
+# copies for backwards compatibility
+cp build/integrating.html build/integrating-foss.html
+cp build/introduction.html build/intro-to-foss.html
